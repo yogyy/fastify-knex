2d22599fb64652caf4e3d4b72536e96748222be1
yogyy <yogimr261@gmail.com>
2024-03-29 06:55:29 +0700

    feat: add libs

--- src/libs/utils.js
+++ src/libs/utils.js
@@ -1,4 +1,5 @@
-/** @typedef { import('fastify').FastifyRequest & { generateToken: Promise<string>} } Request */
+/** @typedef { import('fastify').FastifyRequest  } Request */
+/** @typedef { import('fastify').FastifyReply  } Reply */
 /**
  * @typedef {Object} User
  * @property {string} id
@@ -7,26 +8,55 @@
  */
 
 /**
- * @param { Request } req
+ * @param { Request &  { generateToken: (payload: Object, expiresIn: string) => Promise<string>} } req
  * @param { User } user
  * @returns {Promise<string>} The access token.
  */
 export const generateAccToken = async (req, user) => {
   try {
-    const accessToken = await req.generateToken({
-      sub: user.id,
-      email: user.email,
-      username: user.username,
-    });
-    const refreshToken = await req.generateToken({
-      sub: user.id,
-      email: user.email,
-      username: user.username,
-    });
+    const accessToken = await req.generateToken(
+      {
+        sub: user.id || user.sub,
+        email: user.email,
+        username: user.username,
+      },
+      "1m"
+    );
+    const refreshToken = await req.generateToken(
+      {
+        sub: user.id || user.sub,
+        email: user.email,
+        username: user.username,
+      },
+      "5m"
+    );
 
-    return accessToken;
+    return { accessToken, refreshToken };
   } catch (error) {
     console.error("Error generating access token:", error);
     throw new Error("Failed to generate access token");
   }
+};
+
+/**
+ * @param { Request } req
+ * @param { Reply } reply
+ * @param { User } user
+ */
+export const generateAndSetTokens = async (req, reply, user) => {
+  const { accessToken, refreshToken } = await generateAccToken(req, user);
+
+  reply
+    .setCookie("accessToken", accessToken, {
+      path: "/",
+      httpOnly: true,
+      expires: new Date(Date.now() + 1 * 60 * 1000),
+      secure: true,
+    })
+    .setCookie("refreshToken", refreshToken, {
+      path: "/",
+      httpOnly: true,
+      expires: new Date(Date.now() + 5 * 60 * 1000),
+      secure: true,
+    });
 };
